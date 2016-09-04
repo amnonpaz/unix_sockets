7925f70ae6ec4d95467280f12c6e0d5ab69b4956
Amnon Paz <pazamnon@gmail.com>
2016-09-04 21:31:18 +0300

    Fix First commit
    Add defs.h

---
+++ src/include/defs.h
@@ -0,0 +1,27 @@
+#ifndef __UNIX_DOMAIN_SOCKETS_DEMO_DEFS_H__
+#define __UNIX_DOMAIN_SOCKETS_DEMO_DEFS_H__
+
+#include <sys/un.h>
+#include <stdio.h>
+
+#ifdef SERVICE_NAME
+#define LOG(msg, ...) printf("[" SERVICE_NAME "] " msg "\n", ##__VA_ARGS__)
+#else
+#define LOG(msg, ...) printf(msg "\n", ##__VA_ARGS__)
+#endif
+
+#define LOGE(msg, ...) LOG("ERROR " msg, ##__VA_ARGS__)
+#define LOGI(msg, ...) LOG("INFO  " msg, ##__VA_ARGS__)
+
+#define MAX(a, b) (((a) > (b)) ? a : b)
+#define MIN(a, b) (((a) < (b)) ? a : b)
+
+#define SOCKET_NAME_MAX_LEN \
+    (sizeof(struct sockaddr_un) - sizeof(sa_family_t))
+
+#define SOCKET_NAME_LEN(name) \
+    MIN(strlen(name, SOCKET_NAME_MAX_LEN))
+
+#define MAX_BUFFER_LEN 4096
+
+#endif // __UNIX_DOMAIN_SOCKETS_DEMO_DEFS_H__
